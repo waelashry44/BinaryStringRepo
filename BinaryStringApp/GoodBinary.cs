f51be29bdf87f7413004edacd34e3a2e00ecac0e
wael.ashry@emiratesauction.ae <wael.ashry@emiratesauction.ae>
2020-09-06 13:19:54 +0200

    Add project files.

---
+++ BinaryStringApp/GoodBinary.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BinaryStringTask
+{
+    public class GoodBinary
+    {
+        public bool IsGoodBinaryString(List<char> str, int n)
+        {
+            int CountOfOne = 0;
+            int CountOfZero = 0;
+            bool res = false;
+            CountOfOne = str.Count(s => s == '1');
+            CountOfZero = str.Count(s => s == '0');
+            if (CountOfOne != CountOfZero)
+            {
+                res = false;
+                return res;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                List<char> pref = str.Take(i + 1).ToList();
+                CountOfOne = pref.Count(s => s == '1');
+                CountOfZero = pref.Count(s => s == '0');
+                if (CountOfOne < CountOfZero)
+                {
+                    res = false;
+                    return res;
+                }
+            }
+            res = true;
+            return res;
+        }
+    }
+}
---
+++ BinaryStringApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BinaryStringTask
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            GoodBinary count = new GoodBinary();
+            string input = Console.ReadLine();
+            List<char> str = input.ToCharArray().ToList();
+            int n = str.Count;
+            string result = string.Format("Is Good Binary String : {0}", count.IsGoodBinaryString(str, n));
+            Console.WriteLine(result);
+        }
+    }
+}
